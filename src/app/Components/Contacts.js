6d6b4e0ea1674df0ebe0a4452f7b11884b8e5545
Abdelrahman Maher <Abdelrahmanmaher858@gmail.com>
2025-07-20 00:02:35 +0300

    name

--- src/app/Components/Contacts.js
+++ src/app/Components/Contacts.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Contact from "./Contact";
 import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -10,33 +10,46 @@ export default function Contacts() {
     return (
         <>
             <Container style={{ color: "#D9EFDE" }}>
-                <div
-                    className="d-flex align-items-center justify-content-start"
-                    style={{ gap: "30px" }}
-                >
-                    <Contact />
-                    <a
-                        href="https://www.facebook.com/share/15bBaWx27H/"
-                        target="_blank"
-                        style={{ color: "#D9EFDE" }}
+                <Row>
+                    <Col xs="6">
+                        <h2>Abdelrahman Maher</h2>
+                    </Col>
+                    <Col
+                        xs="6"
+                        style={{
+                            display: "flex",
+                            justifyContent: "end",
+                        }}
                     >
-                        <FacebookRoundedIcon />
-                    </a>
-                    <a
-                        href="https://www.linkedin.com/in/abdelrahmanmaher/"
-                        target="_blank"
-                        style={{ color: "#D9EFDE" }}
-                    >
-                        <LinkedInIcon />
-                    </a>
-                    <a
-                        href="https://github.com/Abdelrahman-maher2000"
-                        target="_blank"
-                        style={{ color: "#D9EFDE" }}
-                    >
-                        <GitHubIcon />
-                    </a>
-                </div>
+                        <div
+                            className="d-flex align-items-center justify-content-start"
+                            style={{ gap: "30px" }}
+                        >
+                            <Contact />
+                            <a
+                                href="https://www.facebook.com/share/15bBaWx27H/"
+                                target="_blank"
+                                style={{ color: "#D9EFDE" }}
+                            >
+                                <FacebookRoundedIcon />
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/abdelrahmanmaher/"
+                                target="_blank"
+                                style={{ color: "#D9EFDE" }}
+                            >
+                                <LinkedInIcon />
+                            </a>
+                            <a
+                                href="https://github.com/Abdelrahman-maher2000"
+                                target="_blank"
+                                style={{ color: "#D9EFDE" }}
+                            >
+                                <GitHubIcon />
+                            </a>
+                        </div>
+                    </Col>
+                </Row>
             </Container>
         </>
     );
